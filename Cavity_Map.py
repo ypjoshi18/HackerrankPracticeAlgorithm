5f22f59c5bd4af262dc1690ebee8d570990296d4
ypjoshi18 <57494405+ypjoshi18@users.noreply.github.com>
2020-08-11 18:51:11 +0530

    Create Cavity_Map.py

---
+++ Cavity_Map.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the cavityMap function below.
+def cavityMap(grid):
+    grid = [list(x) for x in grid]
+    for i in range (1, n-1):
+        for j in range ( 1 , n-1):
+            if grid[i][j] > grid[i-1][j] and grid[i][j] > grid[i+1][j] and grid[i][j] > grid[i][j-1] and grid[i][j] > grid[i][j+1]:
+                grid[i][j]="X"
+        
+    return ["".join(x) for x in grid]
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    grid = []
+
+    for _ in range(n):
+        grid_item = input()
+        grid.append(grid_item)
+
+    result = cavityMap(grid)
+
+    fptr.write('\n'.join(result))
+    fptr.write('\n')
+
+    fptr.close()
